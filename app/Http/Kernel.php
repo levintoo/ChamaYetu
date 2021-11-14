97dd89abf4e5601b40ee21ff1adde4dab659351b
levintoo <tooklevin@gmail.com>
2021-11-14 17:32:36 +0300

    sec+admin+user auth

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -54,6 +54,8 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
+        'authadmin' => \App\Http\Middleware\AuthAdmin::class,
+        'authsecretary' => \App\Http\Middleware\AuthSecretary::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
